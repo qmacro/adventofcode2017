bdb3828f0da5b9429fdb1028656c9fc1dd8ad483
DJ Adams <dj.adams@pobox.com>
2017-12-11 16:52:33 +0000

    solved 8A

--- day8/a.js
+++ day8/a.js
@@ -2,16 +2,55 @@
 // Day 8 Puzzle A
 
 const
-	R = require('ramda'),
+R = require('ramda'),
 
-	// Solution
-	solve = x => {
+// Solution
+solve = x => {
 
-		return true;
+	"use strict";
 
-	};
+	const
+		operations = {
+			"inc" : R.add,
+			"dec" : R.compose(R.add, R.negate),
+			">"   : R.gt,
+			">="  : R.gte,
+			"<"   : R.lt,
+			"<="  : R.lte,
+			"=="  : R.equals,
+			"!="  : R.complement(R.equals)
+		},
+		parseStatement = x => {
+			let matches = x.match(/^(\w+) (inc|dec) (-?\d+) if (\w+) ([!<>=]+) (-?\d+)$/);
+
+			return {
+				reg : matches[1],
+				op : operations[matches[2]](matches[3] * 1),
+				cond: {
+					reg : matches[4],
+					op : operations[matches[5]],
+					val : matches[6] * 1
+				}
+			};
+
+		};
+
+	return R.pipe(
+		R.split(/\n/),
+		R.map(parseStatement),
+		R.reduce((registers, s) => {
+			registers[s.reg] = s.cond.op(registers[s.cond.reg] || 0, s.cond.val)
+				? s.op(registers[s.reg] || 0)
+				: registers[s.reg] || 0;
+			return registers;
+		}, {}),
+		R.values,
+		R.reduce(R.max, 0)
+	)(x)
+
+};
 
 // Invoke solution on input
-require('fs').readFile(__dirname + '/testinput.dat', 'utf8', (err, data) => {
+require('fs').readFile(__dirname + '/input.dat', 'utf8', (err, data) => {
 	console.log(solve(data));
 });
